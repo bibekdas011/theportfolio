52958680bb07799b1f817f6c741db0bd817a0fcc
bibekdas011 <bibek.2500@gmail.com>
2021-10-08 21:32:26 -0700

    uptodate

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    
+  </head>
+  <body>
+  <header>
+    <?php echo '<p>Hello World!Here are all of my Links[Bibek Das Per 7]</p>'; ?> 
+    <div class="container">
+    
+    <nav class="nav nav-pills nav-fill">
+      <a href="index.html">Home </a></li>
+      <br>
+     
+      <a class="nav item nav link active"href="Last_AboutMe.html"> AboutMe</a></li>
+      <br>
+      <a class="nav item nav link" href="Last_Projects.html"> Projects</a></li>
+      <br>
+      <a class="nav item nav link "href="Last_Resume.html"> Resume</a></li>
+      <br>
+      <a class="nav item nav link" href="Last_Contact"> ContactMe</a></li> 
+      <br>
+      
+    </nav>
+    </div>
+    </header>
+  </body>
+</html>
